2da14d883d7df7c0e7fc6e64b7cf4431294e5cce
tsxc <a2166979897@163.com>
2023-01-06 16:25:37 +0800

    更改文件目录结构

--- cpp_to_md.py
+++ cpp_to_md.py
@@ -2,14 +2,27 @@
 import os
 import shutil
 try:
-    shutil.rmtree("./docs/学习笔记/代码/md")
+    os.mkdir("./docs/学习笔记/代码/md")
 except:
     pass
-os.mkdir("./docs/学习笔记/代码/md")
 
-files=os.walk("./docs/学习笔记/代码/cpp")
+
+files=os.walk("./docs")
 for path,dir_list,file_list in files:
     for file in file_list:
-        open(("./docs/学习笔记/代码/md"+'/'+os.path.splitext(file)[0]+".md"), "w", encoding='UTF-8').write("```cpp\n"+open(path+'/'+file,'r', encoding='UTF-8').read()+"```\n")
-        print(os.path.join(path, file) )
+        if(os.path.splitext(file)[-1]==".cpp"):
+            try:
+                md=open(path+'/'+os.path.splitext(file)[0]+".md",'x',encoding="UTF-8")
+            except:
+                md=open(path+'/'+os.path.splitext(file)[0]+".md",'r',encoding="UTF-8")
+                cpp=open(path+'/'+file,'r',encoding="UTF-8")
+                if md.read() != cpp.read():
+                    print("ERROR!Cannot create md file")
+                    print(path+'/'+file)
+                    exit(0)
+                else:
+                    continue
+            cpp=open(path+'/'+file,'r',encoding="UTF-8")
+            md.write(cpp.read())
+            print(path+'/'+file)
 print("cpp to md success")
