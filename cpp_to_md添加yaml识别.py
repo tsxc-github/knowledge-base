b6f09c1e4323c3e1c918137124ff19dd97e96f99
tsxc <a2166979897@163.com>
2023-01-09 13:52:43 +0800

    准备添加yaml识别

---
+++ cpp_to_md添加yaml识别.py
@@ -0,0 +1,60 @@
+# -*- coding: UTF-8 -*-
+import os
+import yaml 
+def md_to_yaml(md_file):
+    first=md_file.readline()
+    if first!="---\n":
+        return ""
+    first=md_file.readline()
+    yaml_file=""
+    while first!="---\n":
+        yaml_file+=first
+        first=md_file.readline()
+        if first=="":
+            return ""
+    return yaml_file
+
+log=open("./cpp_to_md_log.txt",'w',encoding="utf-8")
+
+accept=False
+files=os.walk("./docs")
+for path,dir_list,file_list in files:
+    for file in file_list:
+        if(os.path.splitext(file)[-1]==".cpp"):
+            try:
+                md=open(path+'/'+os.path.splitext(file)[0]+".md",'x',encoding="UTF-8")
+            except:
+                md=open(path+'/'+os.path.splitext(file)[0]+".md",'r',encoding="UTF-8")
+                yaml_file=yaml.safe_load(md_to_yaml(md))
+                if type(yaml_file)=="NoneType" or yaml_file.has_key('temp')==False :
+                    log.write("ERROR!Cannot create md file")
+                    log.write(path+'/'+file)
+                    if accept==True:
+                        log.write("auto cover")
+                        md.close()
+                        md=open(path+'/'+os.path.splitext(file)[0]+".md",'w',encoding="UTF-8")
+                    else:
+                        print("ERROR!Cannot create md file",path+'/'+file)
+                        a=input("Enter \"Y\" to cover,\nEnter \"^C\" to stop,\nEnter \"ALL\" to accept:")
+                        if a=="ALL":
+                            accept=True
+                        if a=="Y" or a=="ALL":
+                            log.write("cover")
+                            md.close()
+                            md=open(path+'/'+os.path.splitext(file)[0]+".md",'w',encoding="UTF-8")
+                            pass
+                        else:
+                            exit(0)
+                else:
+                    continue
+            print(path+'/'+file)
+            cpp=open(path+'/'+file,'r',encoding="UTF-8")
+            md.write("---\ntemp: true\n---\n"+"```cpp\n"+cpp.read()+"\n```")
+log.close()
+log=open("./cpp_to_md_log.txt",'r',encoding="utf-8")
+if log.read()=="":
+    log.close()
+    log=open("./cpp_to_md_log.txt",'w',encoding="utf-8")
+    log.write("run success")
+print("cpp to md success")
+exit(0)
