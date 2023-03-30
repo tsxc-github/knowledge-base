1f0f3d17e9a62ea277efda2f63bad138ff6ad889
tsxc <a2166979897@163.com>
2023-03-30 20:14:59 +0800

    刷题: p1352未完成

---
+++ docs/Learning  Notes/code/p1352.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long
+int main() {
+    LL n;
+    cin >> n;
+    LL r[n + 1];
+    for(LL i = 1; i <= n; i++) {
+        cin >> r[i];
+    }
+    LL zombie[n + 1] = {};
+    LL killer[n + 1] = {};
+    for(LL i = 1; i < n; i++) {
+        LL a, b;
+        cin >> a >> b;
+        killer[b]++;
+        zombie[a] = b;
+    }
+    LL dp[n + 1][2] = {};
+
+    // queue< LL > q;
+    // for(LL i = 1; i <= n; i++) {
+    //     if(killer[i] == 0) {
+    //         q.push(i);
+    //     }
+    // }
+    // LL p;
+    // while(!q.empty()) {
+    //     p = q.front();
+    //     q.pop();
+    //     dp[p][1] += r[p];
+    //     killer[zombie[p]]--;
+    //     if(killer[zombie[p]] == 0) {
+    //         if(zombie[p] != 0)
+    //             q.push(zombie[p]);
+    //     }
+    //     dp[zombie[p]][0] =
+    //         max(dp[zombie[p]][0] + dp[p][0], dp[zombie[p]][0] + dp[p][1]);
+    //     dp[zombie[p]][1] = max(dp[zombie[p]][1], dp[p][0]);
+    // }
+    // LL ans = 0;
+    // ans = max(ans, dp[p][1]);
+    // ans = max(ans, dp[p][0]);
+    
+    cout << ans;
+    return 0;
+}
