af82096a336cd3edc4a47050ccadc52f94d1b48e
tsxc <a2166979897@163.com>
2023-04-01 15:12:47 +0800

    刷题:AC P1352 没有上司的舞会

--- docs/Learning  Notes/code/p1352.cpp
+++ docs/Learning  Notes/code/p1352.cpp
@@ -18,30 +18,31 @@ int main() {
     }
     LL dp[n + 1][2] = {};
 
-    // queue< LL > q;
-    // for(LL i = 1; i <= n; i++) {
-    //     if(killer[i] == 0) {
-    //         q.push(i);
-    //     }
-    // }
-    // LL p;
-    // while(!q.empty()) {
-    //     p = q.front();
-    //     q.pop();
-    //     dp[p][1] += r[p];
-    //     killer[zombie[p]]--;
-    //     if(killer[zombie[p]] == 0) {
-    //         if(zombie[p] != 0)
-    //             q.push(zombie[p]);
-    //     }
-    //     dp[zombie[p]][0] =
-    //         max(dp[zombie[p]][0] + dp[p][0], dp[zombie[p]][0] + dp[p][1]);
-    //     dp[zombie[p]][1] = max(dp[zombie[p]][1], dp[p][0]);
-    // }
-    // LL ans = 0;
-    // ans = max(ans, dp[p][1]);
-    // ans = max(ans, dp[p][0]);
-    
+    queue< LL > q;
+    for(LL i = 1; i <= n; i++) {
+        if(killer[i] == 0) {
+            q.push(i);
+        }
+    }
+    LL p;
+    while(!q.empty()) {
+        p = q.front();
+        q.pop();
+        dp[p][1] += r[p];
+        killer[zombie[p]]--;
+        if(killer[zombie[p]] == 0) {
+            q.push(zombie[p]);
+        }
+        dp[zombie[p]][0] =
+            max(dp[zombie[p]][0] + dp[p][0], dp[zombie[p]][0] + dp[p][1]);
+        dp[zombie[p]][1] += max(dp[zombie[p]][1], dp[p][0]);
+    }
+    LL ans = 0;
+    for(LL i = 0; i <= n; i++) {
+        ans = max(ans, dp[i][1]);
+        ans = max(ans, dp[i][0]);
+    }
+
     cout << ans;
     return 0;
 }
