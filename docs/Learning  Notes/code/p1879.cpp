1a96bc6a89fcb627f9164db81b9eb16f65c2581b
tsxc <a2166979897@163.com>
2023-04-15 16:44:38 +0800

    刷题: AC p1879

--- docs/Learning  Notes/code/p1879.cpp
+++ docs/Learning  Notes/code/p1879.cpp
@@ -11,7 +11,7 @@ int main() {
     LL n, m;
     cin >> m >> n;
 
-    LL grow[m];
+    LL grow[m+1];
     for(LL i = 0; i < m; i++) {
         LL t = 0;
         for(LL j = 0; j < n; j++) {
@@ -19,26 +19,28 @@ int main() {
             cin >> temp;
             t <<= 1;
             t += temp;
+            //			if(j==0) t=temp;
+            //			else t=(t<<1)+temp;
         }
         grow[i] = t;
     }
 
     // dp[i][j]:行数为i,状态为j时的方案数
-    LL dp[m + 1][(1 << (n + 1))] = {};
+    LL dp[m + 1][(1 << n)] = {};
+    // dp[0][0] = 1;
     for(LL j = 0; j < (1 << n); j++) {
         if((grow[0] & j) != j)
             continue;
         dp[0][j] = 1;
     }
-
     for(LL i = 1; i <= m; i++) {
         for(LL j = 0; j < (1 << n); j++) {
             if((grow[i] & j) != j || contiguous(j) == true)
                 continue;
             //			dp[i][j] = 1;
             for(LL k = 0; k < (1 << n); k++) {
-                if((j & k) == 0 && contiguous(k) == false &&
-                   (grow[i - 1] & k) == k) {
+                if((grow[i - 1] & k) == k && (j & k) == 0 &&
+                   contiguous(k) == false) {
                     dp[i][j] = dp[i][j] + dp[i - 1][k];
                     dp[i][j] %= MOD;
                 }
@@ -46,11 +48,6 @@ int main() {
         }
     }
 
-    LL ans = 0;
-    for(LL i = 0; i < (1 << n); i++) {
-        ans += dp[m][i];
-        ans %= MOD;
-    }
-    cout << ans;
+    cout << dp[m][0];
     return 0;
 }
