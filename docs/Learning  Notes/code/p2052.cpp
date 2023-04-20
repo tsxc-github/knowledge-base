11422a0907928f938f7efb59ffeeafc31865e223
tsxc <a2166979897@163.com>
2023-04-20 19:47:26 +0800

    刷题: AC p2052

--- docs/Learning  Notes/code/p2052.cpp
+++ docs/Learning  Notes/code/p2052.cpp
@@ -1,28 +1,54 @@
-#include<iostream>
-#include<cstdio>
-#include<iomanip>
-#include<algorithm>
-#include<cstring>
-#include<stack>
-#include<cmath>
-#include<cctype>
-#include<map>
-#include<queue>
-#include<vector>
-#define LL long long
-#define LD long double
-#define US unsigned short
+#include <bits/stdc++.h>
 using namespace std;
-#define MAX 100
-#ifdef ONLINE_JUDGE
-#define MAX 114514
-#endif
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+#define LL long long
+
+int main() {
     LL n;
-    cin>>n;
-    LL a[n+1];
-    LL b[n+1];
-    LL c[n+1];
+    cin >> n;
+    struct rode
+    {
+        LL a, b, c;
+    };
+
+    vector< rode > a(n + 1);
+    vector< vector< LL > > to(n + 1);
+
+    for(LL i = 1; i < n; i++) {
+        cin >> a[i].a >> a[i].b >> a[i].c;
+        to[a[i].a].push_back(a[i].b);
+        to[a[i].b].push_back(a[i].a);
+    }
+
+    LL root = 1;
+    vector< bool > vis(n + 1, false);
+    vector< LL > father(n + 1, 0);
+    vector< LL > sons(n + 1, 1);
+    function< void(LL) > dfs = [&](LL p) -> void {
+        if(vis[p] == true)
+            return;
+        vis[p] = true;
+        for(auto i : to[p]) {
+            if(vis[i] != true) {
+                dfs(i);
+                father[i] = p;
+                sons[p] += sons[i];
+            }
+        }
+    };
+
+    dfs(root);
+
+    LL ans = 0;
+    for(LL i = 1; i < n; i++) {
+        ans += a[i].c * (abs((n - min(sons[a[i].a], sons[a[i].b])) - min(sons[a[i].a], sons[a[i].b])));
+    }
+#ifndef ONLINE_JUDGE
+    for(LL i = 1; i <= n; i++) {
+        cout << sons[i] << ' ';
+    }
+    cout << endl;
+#endif
+
+    cout << ans;
+    return 0;
 }
